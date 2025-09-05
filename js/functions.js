bdb204fb6e0a22cf0dc067abbbe9c847cc1b50d9
Vadim Voronin <vadim.voronin@gmail.com>
2025-09-05 22:53:20 +0300

    Вторая домашка

---
+++ js/functions.js
@@ -0,0 +1,28 @@
+function stringLength(string, maxLength) {
+  if (string.length <= maxLength){
+    return true;
+  }
+  return false;
+}
+
+
+function isPalimdor(string){
+  let simpleString = string.replaceAll(' ', '').toLowerCase();
+  let stringReverse = '';
+
+  for (let i = simpleString.length - 1; i >= 0; i--){
+    stringReverse += simpleString[i];
+  }
+  return stringReverse === simpleString;
+}
+
+function numberInString(string){
+  string.toString();
+  let number = '';
+  for (let i = 0; i <= string.length; i++){
+    if (+string[i]){
+      number += string[i];
+    }
+  }
+  return number ? Number(number) : NaN;
+}
