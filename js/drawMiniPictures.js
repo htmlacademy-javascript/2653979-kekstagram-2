7017b6789ec316c051ac59a6bcb373eaeac289b0
Vadim Voronin <vadim.voronin@gmail.com>
2025-09-14 21:41:43 +0300

    Исправленна ошибка с шаблоном

--- js/drawMiniPictures.js
+++ js/drawMiniPictures.js
@@ -6,11 +6,11 @@ const pictureListFragment = document.createDocumentFragment();
 posts.forEach((element) => {
   console.log('sdfsdf')
   const post = pictureTemplate.cloneNode(true);
-  pictureTemplate.querySelector('.picture__img').src = element.url;
-  pictureTemplate.querySelector('.picture__img').alt = element.description;
-  pictureTemplate.querySelector('.picture__likes').textContent = element.likes;
-  pictureTemplate.querySelector('.picture__comments').textContent = element.comments.length;
+  post.querySelector('.picture__img').src = element.url;
+  post.querySelector('.picture__img').alt = element.description;
+  post.querySelector('.picture__likes').textContent = element.likes;
+  post.querySelector('.picture__comments').textContent = element.comments.length;
   pictureListFragment.appendChild(post);
 });
 
-pictureList.appendChild(pictureListFragment);
+pictureList.append(pictureListFragment);
